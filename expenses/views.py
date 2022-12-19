87aef55e1ba84040bc65ac0afe1407d6902f39d0
rjpking92 <roody.jerry@yahoo.com>
2022-12-18 20:38:54 -0500

    added template ideal budget

--- expenses/views.py
+++ expenses/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def idealbudget(request):
+    template = loader.get_template('ideal.html')
+    return HttpResponse(template.render())
